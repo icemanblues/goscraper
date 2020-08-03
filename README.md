493c5535d22b22aded50bb39a63aaa4a256eecce
Roland Kluge <roland.kluge@gmail.com>
2020-08-02 22:39:10 -0400

    command line argument and flag support
    * beer
    * links
    * images
    * videos

--- README.md
+++ README.md
@@ -3,3 +3,30 @@
 This is a simple command line utility to scrape web pages.
 
 It aims to use the golang core libraries as much as possible.
+
+## Command Line Arguments
+
+Flags
+
+* beer
+
+```
+$ go-scraper -beer czechvar
+2020/08/02 22:32:43 czechvar : 80
+```
+
+The following flags also require a command line arg of the url to scrape
+
+* links
+* images
+* videos
+
+```
+$ go-scraper -links https://www.github.com/icemanblues/go-scraper
+```
+
+You can also combine one or more of the url based flags.
+
+```
+$ go-scraper -links -images -videos https://www.github.com/icemanblues/go-scraper
+```
--- main.go
+++ main.go
@@ -1,26 +1,70 @@
 package main
 
 import (
+	"flag"
 	"log"
 )
 
-func main() {
-	runBeer()
-}
+// define all command line args here
+var beerFlag string
+var imgFlag bool
+var vidFlag bool
+var linkFlag bool
 
-func runBeer() {
-	beers := []string{
-		"voodoo+ranger",
-		"sloop+juice+bomb",
-		"budweiser",
-		"heineken",
-		"hoegaarden",
-		"miller+lite",
-		"rogue+dead+guy",
-		"stone+ipa",
+func main() {
+	flag.StringVar(&beerFlag, "beer", "", "the name of the beer that you want its ranking")
+	flag.BoolVar(&imgFlag, "images", false, "find all the images on a web site")
+	flag.BoolVar(&vidFlag, "videos", false, "find all the videos on a web site")
+	flag.BoolVar(&linkFlag, "links", false, "find all hyperlinks on a web site")
+	flag.Parse()
+
+	if beerFlag != "" {
+		score := BeerScore(beerFlag)
+		log.Printf("%v : %v\n", beerFlag, score)
 	}
-	for _, beer := range beers {
-		score := beerScore(beer)
-		log.Printf("%v has a score of %v\n", beer, score)
+
+	// at this point, the flags we support must have a command line arg that is the URL
+	url := flag.Arg(0)
+	if url == "" {
+		return
+	}
+
+	if imgFlag {
+		images, err := FindImages(url)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println("images:")
+		for i, img := range images {
+			log.Printf("%d : %s\n", i, img)
+		}
+	}
+
+	if vidFlag {
+		vids, err := FindVideos(url)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println("videos:")
+		for i, v := range vids {
+			log.Printf("%d : %s\n", i, v)
+		}
+	}
+
+	if linkFlag {
+		links, err := FindLinks(url)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println("links:")
+		for i, l := range links {
+			log.Printf("%d : %s\n", i, l)
+		}
 	}
 }
