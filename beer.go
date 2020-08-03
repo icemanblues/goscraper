b772c8a89ce61c1a7ca45979f11bcd992f4d44ee
Roland Kluge <roland.kluge@gmail.com>
2020-08-02 22:38:32 -0400

    Making a better public utility
    BeerScore function is now publically available

--- beer.go
+++ beer.go
@@ -8,8 +8,8 @@ import (
 const BeerURL = "https://www.beeradvocate.com/search/?q="
 const BeerSelector = "#ba-content > div:nth-child(3) > div:nth-child(1) > span"
 
-// callee must URL encode the beerName properly
-func beerScore(beerName string) string {
+// BeerScore callee must URL encode the beerName properly
+func BeerScore(beerName string) string {
 	log.Println(BeerURL + beerName)
 	element, _ := Scrape(BeerURL+beerName, BeerSelector)
 	html, _ := element.Html()
@@ -17,4 +17,22 @@ func beerScore(beerName string) string {
 	endPart := parts[len(parts)-1]
 	pipeParts := strings.Split(endPart, "|")
 	return pipeParts[0]
+}
+
+// a simple demo that this works
+func runBeer() {
+	beers := []string{
+		"voodoo+ranger",
+		"sloop+juice+bomb",
+		"budweiser",
+		"heineken",
+		"hoegaarden",
+		"miller+lite",
+		"rogue+dead+guy",
+		"stone+ipa",
+	}
+	for _, beer := range beers {
+		score := BeerScore(beer)
+		log.Printf("%v has a score of %v\n", beer, score)
+	}
 }
